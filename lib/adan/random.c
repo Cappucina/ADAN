2e8550566fc60fa0529f3a21ac7b96a9ab0f9e40
Hanna Skairipa <hanna@hannaskairipa.com>
2025-12-17 10:46:56 -0700

    fixed RNG somewhat. Not sure if all the way rng.adn is the test. need to fix a issue with the tokens Unexpected token '='

--- lib/adan/random.c
+++ lib/adan/random.c
@@ -1,13 +1,30 @@
 #include <stdlib.h>
+#include <time.h>
 
 void set_seed(int seed) {
     srand(seed);
 }
 
+void set_seed_as_current_time() {
+    srand(time(NULL));
+}
+
 int generate_random_int(int min, int max) {
+    max++;
+
+    if (min > max) {
+        int tmp = min;
+        min = max;
+        max = tmp;
+    }
+
     return (rand() % (max - min + 1)) + min;
 }
 
 int generate_random_int_unrestrained() {
-    return rand();
+    // return rand();
+
+    // I got segmentation faults when doing this needs to be limmited
+
+    return generate_random_int(0, 1024);
 }
