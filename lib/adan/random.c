7d8aa4bafc8ea60f78ecfb31634cfea4eb821208
lily <ilovesuno@proton.me>
2025-12-17 18:58:23 +0000

    change functions

--- lib/adan/random.c
+++ lib/adan/random.c
@@ -5,11 +5,7 @@ void set_seed(int seed) {
     srand(seed);
 }
 
-void set_seed_as_current_time() {
-    srand(time(NULL));
-}
-
-int generate_random_int(int min, int max) {
+int randint(int min, int max) {
     if (min > max) {
         int tmp = min;
         min = max;
@@ -17,12 +13,4 @@ int generate_random_int(int min, int max) {
     }
 
     return (rand() % (max - min + 1)) + min;
-}
-
-int generate_random_int_unrestrained() {
-    // return rand();
-
-    // I got segmentation faults when doing this needs to be limmited
-
-    return generate_random_int(0, 1024);
 }
