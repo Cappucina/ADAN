b0c62e3322c39e57ceea6cfac065bfd93394aff8
lily <ilovesuno@proton.me>
2025-12-17 14:56:56 +0000

    remove unneeded files

--- include/util.h
+++ include/util.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
 #ifndef UTIL_H
 #define UTIL_H
 
@@ -10,6 +13,18 @@ static inline unsigned long hash_string(const char *string) {
 	}
 
 	return hash;
+}
+
+static inline const char* cast(const void* input) {
+	static char buf[64];
+	intptr_t ip = (intptr_t)input;
+
+	if (ip > -4096 && ip < 4096) {
+		snprintf(buf, sizeof(buf), "%ld", (long)ip);
+		return buf;
+	}
+
+	return (const char*)input;
 }
 
 #endif
