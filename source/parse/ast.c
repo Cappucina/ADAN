5332342a034a7a8f46cc88d8af0999c69df8eb17
lily <ilovesuno@proton.me>
2026-01-08 20:23:22 +0000

    Add the MGGU agent

--- source/parse/ast.c
+++ source/parse/ast.c
@@ -1,97 +1,97 @@
 #include "ast.h"
 
-ASTNode* ast_new(ASTNodeType type, size_t line, size_t column) {
-    
+ASTNode* ast_new(ASTNodeType type, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_int_literal(int64_t value, size_t line, size_t column) {
-    
+ASTNode* ast_int_literal(int64_t value, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_float_literal(double value, size_t line, size_t column) {
-    
+ASTNode* ast_float_literal(double value, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_bool_literal(int value, size_t line, size_t column) {
-    
+ASTNode* ast_bool_literal(int value, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_string_literal(const char* value, size_t line, size_t column) {
-    
+ASTNode* ast_string_literal(const char* value, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_char_literal(char value, size_t line, size_t column) {
-    
+ASTNode* ast_char_literal(char value, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_identifier(const char* name, size_t line, size_t column) {
-    
+ASTNode* ast_identifier(const char* name, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_type(const char* name, size_t line, size_t column) {
-    
+ASTNode* ast_type(const char* name, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_binary_expr(ASTOperator op, ASTNode* left, ASTNode* right, size_t line, size_t column) {
-    
+ASTNode* ast_binary_expr(ASTOperator op, ASTNode* left, ASTNode* right, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_unary_expr(ASTOperator op, ASTNode* operand, size_t line, size_t column) {
-    
+ASTNode* ast_unary_expr(ASTOperator op, ASTNode* operand, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_assignment_expr(ASTNode* target, ASTNode* value, size_t line, size_t column) {
-    
+ASTNode* ast_assignment_expr(ASTNode* target, ASTNode* value, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_call_expr(ASTNode* callee, ASTList* arguments, size_t line, size_t column) {
-    
+ASTNode* ast_call_expr(ASTNode* callee, ASTList* arguments, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_member_access(ASTNode* object, const char* member, size_t line, size_t column) {
-    
+ASTNode* ast_member_access(ASTNode* object, const char* member, size_t line, size_t column)
+{
 }
 
-ASTNode* ast_array_access(ASTNode* object, ASTNode* index, size_t line, size_t column) {
-    
+ASTNode* ast_array_access(ASTNode* object, ASTNode* index, size_t line, size_t column)
+{
 }
 
 ASTNode* ast_variable_decl(const char* name, ASTNode* type, ASTNode* initializer, size_t line,
-                           size_t column) {
-    
+                           size_t column)
+{
 }
 
-ASTNode* ast_block_stmt(ASTList* statements, size_t line, size_t column) {
-    
+ASTNode* ast_block_stmt(ASTList* statements, size_t line, size_t column)
+{
 }
 
 ASTNode* ast_if_stmt(ASTNode* condition, ASTNode* then_branch, ASTNode* else_branch, size_t line,
-                     size_t column) {
-    
+                     size_t column)
+{
 }
 
-ASTNode* ast_while_stmt(ASTNode* condition, ASTNode* body, size_t line, size_t column) {
-    
+ASTNode* ast_while_stmt(ASTNode* condition, ASTNode* body, size_t line, size_t column)
+{
 }
 
 ASTNode* ast_for_stmt(ASTNode* initializer, ASTNode* condition, ASTNode* increment, ASTNode* body,
-                      size_t line, size_t column) {
-    
+                      size_t line, size_t column)
+{
 }
 
-ASTNode* ast_return_stmt(ASTNode* value, size_t line, size_t column) {
-    
+ASTNode* ast_return_stmt(ASTNode* value, size_t line, size_t column)
+{
 }
 
 ASTNode* ast_function_decl(const char* name, ASTNode* return_type, ASTList* parameters,
-                           ASTBlockStmt body, size_t line, size_t column) {
-    
+                           ASTBlockStmt body, size_t line, size_t column)
+{
 }
 
-ASTList* ast_list_create(ASTNode** items, size_t count) {
-    
+ASTList* ast_list_create(ASTNode** items, size_t count)
+{
 }
 
-void ast_free(ASTNode* node) {
-    
+void ast_free(ASTNode* node)
+{
 }
--- source/parse/parser.c
+++ source/parse/parser.c
@@ -1,176 +1,177 @@
-#include "lexer.h"
 #include "parser.h"
 
 #include <stdbool.h>
 
-Parser* create_parser(Lexer* lexer, ErrorList* errors) {
-    
+#include "lexer.h"
+
+Parser* create_parser(Lexer* lexer, ErrorList* errors)
+{
 }
 
-Parser* create_parser_from_source(const char* source, ErrorList* errors) {
-
+Parser* create_parser_from_source(const char* source, ErrorList* errors)
+{
 }
 
-void free_parser(Parser* parser) {
-
+void free_parser(Parser* parser)
+{
 }
 
-bool parser_at_end(Parser* parser) {
-
+bool parser_at_end(Parser* parser)
+{
 }
 
-Token parser_peek(Parser* parser) {
-
+Token parser_peek(Parser* parser)
+{
 }
 
-Token parser_peek_next(Parser* parser) {
-
+Token parser_peek_next(Parser* parser)
+{
 }
 
-Token parser_next(Parser* parser) {
-
+Token parser_next(Parser* parser)
+{
 }
 
-bool parser_check(Parser* parser, TokenType type) {
-
+bool parser_check(Parser* parser, TokenType type)
+{
 }
 
-bool parser_match(Parser* parser, TokenType type) {
-
+bool parser_match(Parser* parser, TokenType type)
+{
 }
 
-Token parser_consume(Parser* parser, TokenType type, const char* error_message) {
-
+Token parser_consume(Parser* parser, TokenType type, const char* error_message)
+{
 }
 
-ASTNode* parse_expression(Parser* parser) {
-
+ASTNode* parse_expression(Parser* parser)
+{
 }
 
-ASTNode* parse_assignment(Parser* parser) {
-
+ASTNode* parse_assignment(Parser* parser)
+{
 }
 
-ASTNode* parse_logical_or(Parser* parser) {
-
+ASTNode* parse_logical_or(Parser* parser)
+{
 }
 
-ASTNode* parse_logical_and(Parser* parser) {
-
+ASTNode* parse_logical_and(Parser* parser)
+{
 }
 
-ASTNode* parse_bitwise_or(Parser* parser) {
-
+ASTNode* parse_bitwise_or(Parser* parser)
+{
 }
 
-ASTNode* parse_bitwise_xor(Parser* parser) {
-
+ASTNode* parse_bitwise_xor(Parser* parser)
+{
 }
 
-ASTNode* parse_bitwise_and(Parser* parser) {
-
+ASTNode* parse_bitwise_and(Parser* parser)
+{
 }
 
-ASTNode* parse_equality(Parser* parser) {
-
+ASTNode* parse_equality(Parser* parser)
+{
 }
 
-ASTNode* parse_comparison(Parser* parser) {
-
+ASTNode* parse_comparison(Parser* parser)
+{
 }
 
-ASTNode* parse_shift(Parser* parser) {
-
+ASTNode* parse_shift(Parser* parser)
+{
 }
 
-ASTNode* parse_term(Parser* parser) {
-
+ASTNode* parse_term(Parser* parser)
+{
 }
 
-ASTNode* parse_factor(Parser* parser) {
-
+ASTNode* parse_factor(Parser* parser)
+{
 }
 
-ASTNode* parse_unary(Parser* parser) {
-
+ASTNode* parse_unary(Parser* parser)
+{
 }
 
-ASTNode* parse_postfix(Parser* parser) {
-
+ASTNode* parse_postfix(Parser* parser)
+{
 }
 
-ASTNode* parse_call(Parser* parser) {
-
+ASTNode* parse_call(Parser* parser)
+{
 }
 
-ASTNode* parse_primary(Parser* parser) {
-
+ASTNode* parse_primary(Parser* parser)
+{
 }
 
-ASTNode* parse_program(Parser* parser) {
-
+ASTNode* parse_program(Parser* parser)
+{
 }
 
-ASTNode* parse_parameter_list(Parser* parser) {
-
+ASTNode* parse_parameter_list(Parser* parser)
+{
 }
 
-ASTNode* parse_declaration(Parser* parser) {
-
+ASTNode* parse_declaration(Parser* parser)
+{
 }
 
-ASTNode* parse_variable_declaration(Parser* parser) {
-
+ASTNode* parse_variable_declaration(Parser* parser)
+{
 }
 
-ASTNode* parse_argument_list(Parser* parser) {
-
+ASTNode* parse_argument_list(Parser* parser)
+{
 }
 
-ASTNode* parse_struct_declaration(Parser* parser) {
-
+ASTNode* parse_struct_declaration(Parser* parser)
+{
 }
 
-ASTNode* parse_include_directive(Parser* parser) {
-
+ASTNode* parse_include_directive(Parser* parser)
+{
 }
 
-ASTNode* parse_statement(Parser* parser) {
-
+ASTNode* parse_statement(Parser* parser)
+{
 }
 
-ASTNode* parse_block(Parser* parser) {
-
+ASTNode* parse_block(Parser* parser)
+{
 }
 
-ASTNode* parse_if_statement(Parser* parser) {
-
+ASTNode* parse_if_statement(Parser* parser)
+{
 }
 
-ASTNode* parse_while_statement(Parser* parser) {
-
+ASTNode* parse_while_statement(Parser* parser)
+{
 }
 
-ASTNode* parse_for_statement(Parser* parser) {
-
+ASTNode* parse_for_statement(Parser* parser)
+{
 }
 
-ASTNode* parse_return_statement(Parser* parser) {
-
+ASTNode* parse_return_statement(Parser* parser)
+{
 }
 
-ASTNode* parse_break_statement(Parser* parser) {
-
+ASTNode* parse_break_statement(Parser* parser)
+{
 }
 
-ASTNode* parse_continue_statement(Parser* parser) {
-
+ASTNode* parse_continue_statement(Parser* parser)
+{
 }
 
-ASTNode* parse_type(Parser* parser) {
-
+ASTNode* parse_type(Parser* parser)
+{
 }
 
-ASTNode* parse_type_annotation(Parser* parser) {
-
+ASTNode* parse_type_annotation(Parser* parser)
+{
 }
