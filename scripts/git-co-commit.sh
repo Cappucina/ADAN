76760d7c048239bf9c44381d3fe70a5586a0e2a8
lily <transicle@duck.com>
2026-01-13 10:36:20 -0700

    Add co-author Lily to git-co-commit script

--- scripts/git-co-commit.sh
+++ scripts/git-co-commit.sh
@@ -8,6 +8,7 @@ ${2:-""}
 
 Co-authored-by: Hanna Skairipa <HANNA@HANNASKAIRIPA.COM>
 Co-authored-by: Kauht <89326272+KAUHT@USERS.NOREPLY.GITHUB.COM>
+Co-authored-by: Lily <TRANSICLE@DUCK.COM>
 """
 
 git push
