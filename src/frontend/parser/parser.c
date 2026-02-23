d068844019509d35137a944f9d120c0b55b1e0de
lily.transgirls.win <transicle@duck.com>
2026-02-23 16:34:48 +0000

    upd: add base funcs for parser

--- src/frontend/parser/parser.c
+++ src/frontend/parser/parser.c
@@ -67,13 +67,114 @@ void parser_free(Parser* parser)
  * - static void parse_parameter(Parser* parser);
  * - static void parse_function_body(Parser* parser);
  * - static void parse_block(Parser* parser);
- * - static bool consume(Parser* parser, TokenType type, const char* error_message);
- * - static bool match(Parser* parser, TokenType type);
- * - static void synchronize(Parser* parser);
  * 
  */
 
+static void parse_statement(Parser* parser)
+{
+	
+}
+
+static void parse_function_declaration(Parser* parser)
+{
+
+}
+
+static void parse_variable_declaration(Parser* parser)
+{
+
+}
+
+static void parse_import_statement(Parser* parser)
+{
+
+}
+
+static void parse_expression(Parser* parser)
+{
+
+}
+
+static void parse_primary(Parser* parser)
+{
+
+}
+
+static void parse_call(Parser* parser)
+{
+
+}
+
+static void parse_type(Parser* parser)
+{
+
+}
+
+static void parse_parameter_list(Parser* parser)
+{
+
+}
+
+static void parse_parameter(Parser* parser)
+{
+
+}
+
+static void parse_function_body(Parser* parser)
+{
+
+}
+
+static void parse_block(Parser* parser)
+{
+
+}
+
+static bool consume(Parser* parser, TokenType type, const char* error_message)
+{
+	if (match(parser, type))
+	{
+		advance_token(parser);
+		return true;
+	}
+
+	error_expected(parser, error_message);
+	return false;
+}
+
+static bool match(Parser* parser, TokenType type)
+{
+	return peek_current(parser) && peek_current(parser)->type == type;
+}
+
+static void synchronize(Parser* parser)
+{
+	advance_token(parser);
+
+	while (peek_current(parser) && peek_current(parser)->type != TOKEN_SEMICOLON)
+	{
+		switch (peek_current(parser)->type)
+		{
+		case TOKEN_FUN:
+		case TOKEN_IMPORT:
+		case TOKEN_SET:
+			return;
+		default:
+			break;
+		}
+
+		advance_token(parser);
+	}
+
+	if (peek_current(parser) && peek_current(parser)->type == TOKEN_SEMICOLON)
+	{
+		advance_token(parser);
+	}
+}
+
+//  Primary parsing function
+
 void parser_parse_program(Parser* parser)
 {
-    // @todo Implement later
+    
 }
