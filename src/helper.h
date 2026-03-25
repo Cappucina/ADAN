cd5e1beecfd024f4c12747b21919120be3b2e010
transicle <transicle@duck.com>
2026-03-24 17:50:07 -0700

    upd: 2026-03-24-17-50

--- src/helper.h
+++ src/helper.h
@@ -1,6 +1,14 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#ifdef _WIN32
+#include <sys/stat.h>
+#define PATH_MAX 260
+#ifndef S_ISDIR
+#define S_ISDIR(mode) (((mode) & _S_IFMT) == _S_IFDIR)
+#endif
+#endif
+
 #include <stddef.h>
 
 char* read_file(char* file_path);
